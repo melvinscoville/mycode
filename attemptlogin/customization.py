50ff0018b546af926abf7b222c2fad128ddc3459
melvinscoville <melvin.scoville@gmail.com>
2022-09-28 15:01:14 +0000

    Outputting files

--- attemptlogin/customization.py
+++ attemptlogin/customization.py
@@ -14,6 +14,7 @@ with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
         # if this 'fail pattern' appears in the line...
         if "- - - - -] Authorization failed" in line:
             loginfail += 1 # this is the same as loginfail = loginfail + 1
+            print(f"Failed Login from IP Addr:  {line.split(' ')[-1]}")
         # the following elif agumentation MUST be AFTER we check for a fail
         # this statement assumes the if statement above it tested false
         elif "-] Authorization failed" in line:  # can ONLY be true if the "if" was false!
